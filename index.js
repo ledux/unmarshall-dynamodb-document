9f3094f1c39aa672a2920151d34d0c9b7f133834
Lukas Tanner <lukastanner@outlook.com>
2023-06-16 07:11:50 +0200

    set into working state

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const { unmarshall } = require("@aws-sdk/util-dynamodb");
 const fs = require("fs");
 
 program
-  .command("do")
-  .option("-j, --json <jsonInput>", "raw json string as an input")
+  .command("do <jsonInput>")
+  // .option("-j, --json <jsonInput>", "raw json string as an input")
   .description("Remove the data types entries from document")
   .action(clean);
 
-program
-  .command("do")
-  .option("-f, --file <filePath>", "json passed as contents of a file")
-  .description("Remove the data types entries from document")
-  .action(readFile);
+// program
+// .command("do")
+// .option("-f, --file <filePath>", "json passed as contents of a file")
+// .description("Remove the data types entries from document")
+// .action(readFile);
 
 function clean(jsonInput) {
   const json = JSON.parse(jsonInput);
